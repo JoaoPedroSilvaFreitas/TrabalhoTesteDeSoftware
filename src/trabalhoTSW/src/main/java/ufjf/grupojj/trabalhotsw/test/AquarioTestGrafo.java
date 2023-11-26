9782f91a9d67d0b00c2e67e1ce36a3574e060ae8
João <freitas.joaopedro@estudante.ufjf.br>
2023-11-25 23:30:45 -0300

    testes grafo causa-efeito

--- src/trabalhoTSW/src/main/java/ufjf/grupojj/trabalhotsw/test/AquarioTestGrafo.java
+++ src/trabalhoTSW/src/main/java/ufjf/grupojj/trabalhotsw/test/AquarioTestGrafo.java
@@ -7,66 +7,64 @@ import ufjf.grupojj.trabalhotsw.view.main;
 
 import static org.junit.Assert.*;
 
-public class AquarioTest {
+public class AquarioTestGrafo {
 
 	@Test
 	void testeFuncional1(){
 		System.out.println("Teste 1");
 		int var = 0;
-		main.iniciaJogo(0,1,var,var,var,var,var,var);
+		main.iniciaJogo(2, 2, 1, 1, 1, 1, 1, 1);
 	}
-
 
 	@Test
 	void testeFuncional2(){
 		System.out.println("Teste 2");
 		int var = 0;
-		main.iniciaJogo(-1,1,var,var,var,var,var,var);
+		main.iniciaJogo(0,1,var,var, var, var, var,var);
 	}
 	
 	@Test
 	void testeFuncional3(){
 		System.out.println("Teste 3");
 		int var = 0;
-		main.iniciaJogo(1,0,var,var,var,var,var,var);
+		main.iniciaJogo(-1,1,var,var, var, var, var,var);
 	}
-	
+
 	@Test
 	void testeFuncional4(){
 		System.out.println("Teste 4");
 		int var = 0;
-		main.iniciaJogo(1,-1,var,var,var,var,var,var);
+		main.iniciaJogo(1,0,var,var, var, var, var,var);
 	}
-	
+
 
 	@Test
 	void testeFuncional5(){
 		System.out.println("Teste 5");
 		int var = 0;
-		main.iniciaJogo(1,1,-1,0,var,var,var,var);
+		main.iniciaJogo(1,-1,var,var, var, var, var,var);
 	}
 	
-
 	@Test
 	void testeFuncional6(){
 		System.out.println("Teste 6");
 		int var = 0;
-		main.iniciaJogo(1,1,0,-1,var,var,var,var);
+		main.iniciaJogo(2,2,-1,var, var, var, var,var);
 	}
-	
+
+
 	@Test
 	void testeFuncional7(){
 		System.out.println("Teste 7");
 		int var = 0;
-		main.iniciaJogo(1,1,1,1,var,var,var,var);
+		main.iniciaJogo(2,2,-1,var, var, var, var,var);
 	}
-	
 	
 	@Test
 	void testeFuncional8(){
 		System.out.println("Teste 8");
 		int var = 0;
-		main.iniciaJogo(1, 2, 1, 1, -1, 0,var,var);
+		main.iniciaJogo(2,2,1,1,-1, var, var,var);
 	}
 	
 	
@@ -74,28 +72,33 @@ public class AquarioTest {
 	void testeFuncional9(){
 		System.out.println("Teste 9");
 		int var = 0;
-		main.iniciaJogo(1, 2, 1, 1, 0, -1,var,var);
+		main.iniciaJogo(2,2,1,1,1,-1, var,var);
 	}
-	
-	
+
+
 	@Test
 	void testeFuncional10(){
 		System.out.println("Teste 10");
 		int var = 0;
-		main.iniciaJogo(2, 1, 1, 1, 1, 1, -1, 0);
+		main.iniciaJogo(2,2,1,1,1,1,-1,var);
 	}
+	
 	
 	@Test
 	void testeFuncional11(){
 		System.out.println("Teste 11");
 		int var = 0;
-		main.iniciaJogo(2, 1, 1, 1, 1, 1, 0, -1);
+		main.iniciaJogo(2,2,1,1,1,1,1,-1);
 	}
+	
 	
 	@Test
 	void testeFuncional12(){
 		System.out.println("Teste 12");
 		int var = 0;
-		main.iniciaJogo(2, 2, 1, 1, 1, 1, 1, 1);
+		main.iniciaJogo(1,1,1,1,1,1,1,1);
 	}
+
+
+	
 }
